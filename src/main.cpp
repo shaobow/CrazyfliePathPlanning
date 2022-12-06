9b1a6ffa04b999e0ed0a1608c8be216f0e4f1daf
Shaobo Wang <shaobow@andrew.cmu.edu>
2022-12-06 17:27:37 -0500

    enabled matlab pass in

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,9 @@
 #define MAP_ID prhs[0]
 #define GRID_SIZE prhs[1]
 #define MARGIN_SIZE prhs[2]
+#define START prhs[3]
+#define GOAL prhs[4]
+#define USE_DSTAR prhs[5]
 
 /* Output Arguments */
 #define PATH plhs[0]
@@ -22,32 +25,31 @@ namespace {
 const std::string MAP1_PATH = "./maps/map1.txt";
 const std::string MAP2_PATH = "./maps/map2.txt";
 const std::string MAP3_PATH = "./maps/map3.txt";
+const std::string MAP_TEST_PATH = "./maps/map_test.txt";
 }  // namespace
 
-vector<vector<double>> plan(int map_id, double grid_size, double margin_size) {
+vector<vector<double>> plan(int map_id, double grid_size, double margin_size,
+                            double robot_x, double robot_y, double robot_z,
+                            double goal_x, double goal_y, double goal_z,
+                            bool use_dstar = false) {
   std::string map_path;
-  double robot_x, robot_y, robot_z;
-  double goal_x, goal_y, goal_z;
+  vector<vector<double>> solution = {};
 
   switch (map_id) {
     case 1:
       map_path = MAP1_PATH;
-      robot_x = 0.0;
-      robot_y = -4.9;
-      robot_z = 0.2;
-      goal_x = 6.0;
-      goal_y = 17.0;
-      goal_z = 5.0;
+      break;
+
+    case 2:
+      map_path = MAP2_PATH;
       break;
 
     case 3:
       map_path = MAP3_PATH;
-      robot_x = 0.0;
-      robot_y = 5.0;
-      robot_z = 5.0;
-      goal_x = 20.0;
-      goal_y = 5.0;
-      goal_z = 5.0;
+      break;
+
+    case 4:
+      map_path = MAP_TEST_PATH;
       break;
 
     default:
@@ -55,41 +57,89 @@ vector<vector<double>> plan(int map_id, double grid_size, double margin_size) {
       break;
   }
 
-  auto start = std::chrono::high_resolution_clock::now();
-  CF_PLAN::PlannerDstar dstarLite(robot_x, robot_y, robot_z, goal_x, goal_y,
-                                  goal_z, map_path, grid_size, margin_size);
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto construct_time =
-      std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-  std::cout << "D* Lite planner takes " << construct_time.count() / 1000.0
-            << " seconds to construct.\n";
+  if (use_dstar) {
+    auto start = std::chrono::high_resolution_clock::now();
+    CF_PLAN::PlannerDstar dstarLite(robot_x, robot_y, robot_z, goal_x, goal_y,
+                                    goal_z, map_path, grid_size, margin_size);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto construct_time =
+        std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << "D* Lite planner takes " << construct_time.count() / 1000.0
+              << " seconds to construct.\n";
 
-  start = chrono::high_resolution_clock::now();
-  dstarLite.plan();
-  stop = chrono::high_resolution_clock::now();
-  auto solve_time = chrono::duration_cast<chrono::milliseconds>(stop - start);
-  dstarLite.printPath();
-  auto solution = dstarLite.getPath();
-  std::cout << "D* Lite planner takes " << solve_time.count() / 1000.0
-            << " seconds to find solution.\n";
+    start = chrono::high_resolution_clock::now();
+    dstarLite.plan();
+    stop = chrono::high_resolution_clock::now();
+    auto solve_time = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    // dstarLite.printPath();
+    solution = dstarLite.getPath();
+    std::cout << "D* Lite planner takes " << solve_time.count() / 1000.0
+              << " seconds to find solution.\n";
+  } else {
+    auto start = std::chrono::high_resolution_clock::now();
+    CF_PLAN::Planner astar(robot_x, robot_y, robot_z, goal_x, goal_y, goal_z,
+                           map_path, grid_size, margin_size);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto construct_time =
+        std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::cout << "A* planner takes " << construct_time.count() / 1000.0
+              << " seconds to construct.\n";
+
+    start = chrono::high_resolution_clock::now();
+    astar.plan();
+    stop = chrono::high_resolution_clock::now();
+    auto solve_time = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    // astar.printPath();
+    solution = astar.getPath();
+    std::cout << "A* planner takes " << solve_time.count() / 1000.0
+              << " seconds to find solution.\n";
+  }
   return solution;
 }
 
 /* MEX entry function */
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   /* argument check */
-  if (nrhs != 3) {
-    mexErrMsgIdAndTxt("MATLAB:inputmismatch", "Input arguments must be 3!");
+  if (nrhs != 6) {
+    mexErrMsgIdAndTxt("MATLAB:cudaAdd:inputmismatch",
+                      "Input arguments must be 6!");
   }
   if (nlhs != 1) {
-    mexErrMsgIdAndTxt("MATLAB:outputmismatch", "Output arguments must be 1!");
+    mexErrMsgIdAndTxt("MATLAB:cudaAdd:outputmismatch",
+                      "Output arguments must be 1!");
   }
 
   int map_id = (int)mxGetScalar(MAP_ID);
   double grid_size = mxGetScalar(GRID_SIZE);
   double margin_size = mxGetScalar(MARGIN_SIZE);
 
-  auto solution = plan(map_id, grid_size, margin_size);
+  double *start_ptr = mxGetPr(START);
+  double *goal_ptr = mxGetPr(GOAL);
+
+  bool use_dstar = (bool)mxGetScalar(USE_DSTAR);
+
+  if (mxGetM(START) != 3 || mxGetN(START) != 1) {
+    mexErrMsgIdAndTxt("MATLAB:cudaAdd:sizemismatch",
+                      "Input matrices must have size of 3x1!");
+    return;
+  }
+
+  if (mxGetM(GOAL) != 3 || mxGetN(GOAL) != 1) {
+    mexErrMsgIdAndTxt("MATLAB:cudaAdd:sizemismatch",
+                      "Input matrices must have size of 3x1!");
+    return;
+  }
+
+  double robot_x = start_ptr[0];
+  double robot_y = start_ptr[1];
+  double robot_z = start_ptr[2];
+
+  double goal_x = goal_ptr[0];
+  double goal_y = goal_ptr[1];
+  double goal_z = goal_ptr[2];
+
+  auto solution = plan(map_id, grid_size, margin_size, robot_x, robot_y,
+                       robot_z, goal_x, goal_y, goal_z, use_dstar);
   mwSize m = solution.size();
   mwSize n = 3;
   PATH = mxCreateDoubleMatrix(m, n, mxREAL);
--- visualization/runsim.m
+++ visualization/runsim.m
@@ -5,6 +5,7 @@ addpath(genpath('./'));
 addpath(genpath('../bin/'))
 
 map_id = 1;
+use_dstar = 0;
 path = cell(1);
 
 switch map_id
@@ -15,12 +16,13 @@ switch map_id
         map = load_map('maps/map1.txt', grid_size, grid_size, 0.0);
         start = {[0.0  -4.9 0.2]};
         stop  = {[6.0  17.0 5.0]};
-        tic
-          disp('Planning ...');
-          path{1} = cfPlanning(1, grid_size, margin_size);
-        toc
-        
-        plot_path(map, path{1});
+    case 2
+        % Plan path 2
+        grid_size = 0.2;
+        margin_size = 0.2;
+        map = load_map('maps/map2.txt', grid_size, grid_size, 0.0);
+        start = {[0.0  -4.9 0.2]};
+        stop  = {[6.0  17.0 5.0]};
     case 3
         % Plan path 3
         grid_size = 0.2;
@@ -28,14 +30,21 @@ switch map_id
         map = load_map('maps/map3.txt', grid_size, grid_size, 0.0);
         start = {[0.0, 5.0, 5.0]};
         stop  = {[20.0, 5.0, 5.0]};
-        nquad = length(start);
-        tic
-            disp('Planning ...');
-            path{1} = cfPlanning(3, grid_size, margin_size);
-        toc
-        plot_path(map, path{1});
-
+    case 4
+        % Plan path test
+        grid_size = 0.2;
+        margin_size = 0.2;
+        map = load_map('maps/map_test.txt', grid_size, grid_size, 0.0);
+        start = {[8 0.5 2]};
+        stop  = {[17 19.5 8]};
 end
+
+tic
+  disp('Planning ...');
+  path{1} = cfPlanning(map_id, grid_size, margin_size, start{1}', stop{1}',use_dstar);
+toc
+
+plot_path(map, path{1});
 
 %% Additional init script
 init_script;
